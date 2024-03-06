f011575b21ae6125001685033717094f3fc56110
ariel PCportatil <arielyanceprueba@gmail.com>
2024-03-05 23:03:37 -0500

    pagina web de jhoymar

---
+++ codigo.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botonAzul = document.getElementById("boton-azul");
+    const botonRojo = document.getElementById("boton-rojo");
+
+    const imagenesSecundarias = document.querySelectorAll(".ImagenSecundaria img");
+
+    botonAzul.addEventListener("click", function() {
+        imagenesSecundarias.forEach(function(imagen) {
+            imagen.style.animation = "none";
+            setTimeout(function() {
+                imagen.style.animation = "example 2s ease-in-out forwards";
+            }, 100);
+        });
+    });
+
+    botonRojo.addEventListener("click", function() {
+        window.location.href = "https://www.youtube.com/watch?v=_kihwgulr2A"; // Aquí puedes cambiar el enlace por el que desees
+    });
+});
